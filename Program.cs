3cd7784e85cdacb20d446396a30c82fa23d52cd4
Roberto Avila <keepteacher@gmail.com>
2025-03-10 12:12:18 -0600

    Actualizacion de codigo

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace NetAppUsingYield
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            // Consultemos cada metodo
+
+            foreach (var line in ReadLog.ReadLogFile("log.txt"))
+            {
+                Console.WriteLine(line);
+            }
+
+
+            foreach (var line in ReadLog.ReadLogLinesRightWay("log.txt"))
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
---
+++ ReadLogFile.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetAppUsingYield
+{
+    static class ReadLog
+    {
+        /// <summary>
+        /// Esta es la forma en la que no se deberia hacer
+        /// en este ejemplo se utiliza demasiada memoria cuando el log es muy 
+        /// extenso o que excede la capacidad de memoria del host
+        /// </summary>
+        /// <param name="StringPath"></param>
+        /// <returns></returns>
+        public static List<String> ReadLogFile(String StringPath)
+        {
+            var lines = new List<string>();
+            foreach (var line in File.ReadAllLines(StringPath))
+            {
+                if (!string.IsNullOrEmpty(line)) lines.Add(line);
+            }
+            return lines;
+        }
+        /// <summary>
+        /// Esta es la mejor forma de optimizar los recursos de memoria del host
+        /// </summary>
+        /// <param name="StringPath"></param>
+        /// <returns></returns>
+        public static IEnumerable<string> ReadLogLinesRightWay(String StringPath)
+        {
+            foreach (var line in File.ReadLines(StringPath))
+            {
+                if (!string.IsNullOrEmpty(line)) yield return line;
+            }
+        }
+
+    }
+}
